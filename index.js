f398e567611dfae8f118aed87a44dc7f1e5986ba
Matt Hauck <matt.hauck@tanium.com>
2016-06-25 23:51:54 -0700

    Force listen on localhost, ipv4

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function main()  {
     var app = newServer(slack);
 
     app.set('port', (process.env.PORT || 5000));
-    app.listen(app.get('port'), function() {
+    app.listen(app.get('port'), '127.0.0.1', function() {
         console.log('Node app is running on port', app.get('port'));
     });
 }
