996ec2d8ba9ae4c5d06617cb1c1dc31b5e2d5a84
Matt Hauck <matt.hauck@tanium.com>
2017-01-09 22:06:38 -0800

    Add some match guards!

--- src/server/github_verify.rs
+++ src/server/github_verify.rs
@@ -17,7 +17,9 @@ impl fmt::Display for StringError {
 }
 
 impl std::error::Error for StringError {
-    fn description(&self) -> &str { &*self.0 }
+    fn description(&self) -> &str {
+        &*self.0
+    }
 }
 
 pub struct GithubWebhookVerifier {
@@ -28,22 +30,32 @@ impl GithubWebhookVerifier {
     fn is_valid(&self, data: &[u8], signature: &String) -> IronResult<()> {
         // assume it starts with 'sha1='
         if signature.len() < 6 {
-            return Err(IronError::new(StringError("Invalid signature value".to_string()), status::BadRequest));
+            return Err(IronError::new(StringError("Invalid signature value".to_string()),
+                                      status::BadRequest));
         }
         let sig_prefix = &signature[0..5];
         if sig_prefix != "sha1=" {
-            return Err(IronError::new(StringError(format!("Invalid signature value. Expected 'sha1='; found: '{}'", sig_prefix)), status::BadRequest));
+            return Err(IronError::new(StringError(format!("Invalid signature value. Expected \
+                                                           'sha1='; found: '{}'",
+                                                          sig_prefix)),
+                                      status::BadRequest));
         }
 
         let sig_bytes: Vec<u8> = match signature[5..].from_hex() {
             Ok(s) => s,
-            Err(e) => return Err(IronError::new(StringError(format!("Invalid hex value: {}", e)), status::BadRequest)),
+            Err(e) => {
+                return Err(IronError::new(StringError(format!("Invalid hex value: {}", e)),
+                                          status::BadRequest))
+            }
         };
 
         let key = hmac::VerificationKey::new(&digest::SHA1, self.secret.as_bytes());
         match hmac::verify(&key, data, &sig_bytes) {
             Ok(_) => Ok(()),
-            Err(_) => Err(IronError::new(StringError("Invalid signature!".to_string()), status::BadRequest))
+            Err(_) => {
+                Err(IronError::new(StringError("Invalid signature!".to_string()),
+                                   status::BadRequest))
+            }
         }
     }
 }
@@ -51,20 +63,21 @@ impl GithubWebhookVerifier {
 impl BeforeMiddleware for GithubWebhookVerifier {
     fn before(&self, req: &mut Request) -> IronResult<()> {
         let sig_header: String = match req.headers.get_raw("x-hub-signature") {
-            Some(h) => {
-                if h.len() == 0 || h.len() > 1 {
-                    return Err(IronError::new(StringError("Expected to find exactly one signature header".to_string()), status::BadRequest))
-                } else {
-                    String::from_utf8_lossy(&h[0]).into_owned()
-                }
-            },
-            None => return Err(IronError::new(StringError("Expected to find exactly one signature header".to_string()), status::BadRequest)),
+            Some(ref h) if h.len() == 1 => String::from_utf8_lossy(&h[0]).into_owned(),
+            None | Some(..) => {
+                return Err(IronError::new(StringError("Expected to find exactly one signature \
+                                                       header"
+                                              .to_string()),
+                                          status::BadRequest))
+            }
         };
 
         let body = match req.get::<bodyparser::Raw>() {
             Ok(Some(body)) => body,
-            Ok(None) => String::new(),
-            Err(e) => return Err(IronError::new(StringError(format!("Error reading body: {}", e)), status::InternalServerError)),
+            Err(_) | Ok(None) => {
+                return Err(IronError::new(StringError("Error reading body".to_string()),
+                                          status::InternalServerError))
+            }
         };
 
         self.is_valid(body.as_bytes(), &sig_header)
