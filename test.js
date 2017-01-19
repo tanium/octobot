37a3eb7ce57892945eac42fddd37968cdae70a48
Matt Hauck <matt.hauck@tanium.com>
2017-01-18 23:54:54 -0800

    Remove nodejs version

--- test.js
+++
@@ -1,8 +0,0 @@
-var Jasmine = require('jasmine');
-var SpecReporter = require('jasmine-spec-reporter');
-
-var jrunner = new Jasmine();
-jrunner.env.clearReporters();
-jrunner.addReporter(new SpecReporter());
-jrunner.loadConfigFile();
-jrunner.execute();
