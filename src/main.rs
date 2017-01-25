7f30e300b7bc6c394d2cf31fd63174d10b04b54c
Matt Hauck <matt.hauck@tanium.com>
2017-01-25 10:03:35 -0800

    Add timestamps to logging

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,43 @@
 extern crate env_logger;
 extern crate octobot;
+extern crate time;
+
+#[macro_use]
+extern crate log;
 
 use octobot::config;
 use octobot::server;
+
+use log::{LogRecord, LogLevelFilter};
+use env_logger::LogBuilder;
 
 fn main() {
     if std::env::args().len() < 2 {
         panic!("Usage: octobot <config-file>")
     }
 
-    env_logger::init().unwrap();
+    setup_logging();
 
     let config = config::parse(std::env::args().nth(1).unwrap()).expect("Error parsing config");
 
     server::start(config).expect("Failed to start server");
+}
+
+fn setup_logging() {
+    let formatter = |record: &LogRecord| {
+        let t = time::now();
+        format!("{},{:03} - {} - {}",
+                time::strftime("%Y-%m-%d %H:%M:%S", &t).unwrap(),
+                t.tm_nsec / 1000_000,
+                record.level(),
+                record.args())
+    };
+
+    let mut builder = LogBuilder::new();
+    builder.format(formatter).filter(None, LogLevelFilter::Info);
+    if std::env::var("RUST_LOG").is_ok() {
+        builder.parse(&std::env::var("RUST_LOG").unwrap());
+    }
+
+    builder.init().unwrap();
 }
