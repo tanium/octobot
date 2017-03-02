7cc631f26ec29588df9200ba2164f7cb4fe6a3bb
Matt Hauck <matthauck@gmail.com>
2017-03-01 21:27:34 -0800

    Add more diff information on force-push (#61)

--- tests/force_push_test.rs
+++ tests/force_push_test.rs
@@ -116,6 +116,127 @@ fn test_force_push_different() {
 }
 
 #[test]
+fn test_force_push_different_with_details() {
+    let diff0 = r#"
+diff --git a/src/diffs.rs b/src/diffs.rs
+index 9c8643c..5aa6c73 100644
+--- a/src/diffs.rs
++++ b/src/diffs.rs
+@@ -1,3 +1,4 @@
++use std::cmp::max;
+ use unidiff::{PatchSet, PatchedFile, Hunk, Line};
+
+ #[derive(Debug)]
+@@ -36,6 +37,16 @@ impl DiffOfDiffs {
+
+         are_patch_sets_equal(patch0, patch1)
+     }
++
++    pub fn different_patch_files(&self) -> Vec<PatchedFile> {
++        if let Some(ref patch0) = self.patch0 {
++            if let Some(ref patch1) = self.patch1 {
++                return different_patch_files(patch0, patch1)
++            }
++        }
++
++        vec![]
++    }
+ }
+
+ fn parse_diff(diff: &str) -> Option<PatchSet> {
+diff --git a/src/force_push.rs b/src/force_push.rs
+index 33667da..3503c28 100644
+--- a/src/force_push.rs
++++ b/src/force_push.rs
+@@ -29,10 +29,15 @@ pub fn comment_force_push(diffs: Result<DiffOfDiffs, String>,
+                 comment += "Identical diff post-rebase";
+                 identical_diff = true;
+             } else {
+-                // TODO: How to expose this diff -- maybe create a secret gist?
+-                // But that may raise permissions concerns for users who can read octobot's gists,
+-                // but perhaps not the original repo...
+                 comment += "Diff changed post-rebase";
++                let different_files = diffs.different_patch_files();
++                if different_files.len() > 0 {
++                    comment += "\n\nChanged files:\n";
++                    for file in different_files {
++                        comment += &format!("* {}", file.path());
++                    }
++                }
++
+                 identical_diff = false;
+             }
+         }, "#;
+
+    let diff1 = r#"
+diff --git a/src/diffs.rs b/src/diffs.rs
+index 9c8643c..5aa6c73 100644
+--- a/src/diffs.rs
++++ b/src/diffs.rs
+@@ -1,3 +1,4 @@
++use std::cmp::max;
+ use unidiff::{PatchSet, PatchedFile, Hunk, Line};
+
+ #[derive(Debug)]
+@@ -36,6 +37,16 @@ impl DiffOfDiffs {
+
+         are_patch_sets_equal(patch0, patch1)
+     }
++    +++++++++++++++++++++++++++++++++++++++
++    pub fn DIFFERENT_FUNCTION_NAME__MUAHAHA()
++        if let Some(ref patch0) = self.patch0 {
++            if let Some(ref patch1) = self.patch1 {
++                return different_patch_files(patch0, patch1)
++            }
++        }
++
++        vec![]
++    }
+ }
+
+ fn parse_diff(diff: &str) -> Option<PatchSet> {
+diff --git a/src/force_push.rs b/src/force_push.rs
+index 33667da..3503c28 100644
+--- a/src/force_push.rs
++++ b/src/force_push.rs
+@@ -29,10 +29,15 @@ pub fn comment_force_push(diffs: Result<DiffOfDiffs, String>,
+                 comment += "TOTALLY THE SAME!";
+                 identical_diff = true;
+             } else {
+-                // TODO: How to expose this diff -- maybe create a secret gist?
+-                // But that may raise permissions concerns for users who can read octobot's gists,
+-                // but perhaps not the original repo...
+                 comment += "Diff changed post-rebase";
++                let different_files = diffs.different_patch_files();
++                if different_files.len() > 0 {
++                    comment += "\n\nChanged files:\n";
++                    for file in different_files {
++                        comment += &format!("* {}", file.path());
++                    }
++                }
++
+                 identical_diff = false;
+             }
+         }, "#;
+
+    let mut pr = github::PullRequest::new();
+    pr.number = 32;
+
+    let diffs = Ok(DiffOfDiffs::new(diff0, diff1));
+
+    let github = MockGithub::new();
+    github.mock_comment_pull_request("some-user", "some-repo", 32,
+        "Force-push detected: before: abcdef0, after: 1111abc: Diff changed post-rebase\n\n\
+        Changed files:\n\
+        * src/diffs.rs\n\
+        * src/force_push.rs\n\
+        ",
+        Ok(()));
+
+    force_push::comment_force_push(diffs, vec![], &github, "some-user", "some-repo", &pr,
+                                   "abcdef0999999", "1111abc9999999").unwrap();
+}
+#[test]
 fn test_force_push_error() {
     let mut pr = github::PullRequest::new();
     pr.number = 32;
