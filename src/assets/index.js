e3cee50821a27eba12d0e56648d71dec856ba75b
Matt Hauck <matt.hauck@tanium.com>
2017-03-08 11:15:06 -0800

    add some repos

--- src/assets/index.js
+++ src/assets/index.js
@@ -48,12 +48,19 @@ app.controller('AdminController', function($scope, $http) {
     })
   }
 
+  $scope.state = 'users';
   $scope.users = [];
   $scope.repos = [];
 
   http_get('/api/users').then(function(resp) {
     $scope.users = resp.data.users;
 
+    // map it to be more angular friendly
+    for (var host in $scope.users) {
+      for (var username in $scope.users[host]) {
+        $scope.users[host][username]._username = username;
+      }
+    }
   }).catch(function(e) {
     alert("Error getting users: " + e);
   });
@@ -61,6 +68,19 @@ app.controller('AdminController', function($scope, $http) {
   http_get('/api/repos').then(function(resp) {
     $scope.repos = resp.data.repos;
 
+    for (var host in $scope.repos) {
+      for (var repo in $scope.repos[host]) {
+        var info = $scope.repos[host][repo];
+        info._repo = repo;
+
+        if (info.force_push_notify == null)  {
+          info.force_push_notify = true;
+        }
+        if (info.jira_enabled == null)  {
+          info.jira_enabled = true;
+        }
+      }
+    }
   }).catch(function(e) {
     alert("Error getting repos: " + e);
   });
@@ -69,5 +89,52 @@ app.controller('AdminController', function($scope, $http) {
     http_post('/auth/logout', {}).finally(function() {
       delete sessionStorage['session'];
     });
+  }
+
+  $scope.addUser = function(host) {
+    $scope.users[host]["new-user-" + Math.random()] = {};
+  }
+
+  $scope.removeUser = function(host, username) {
+    delete $scope.users[host][username];
+  }
+
+  $scope.saveUsers = function() {
+    // remap to make sure edited usersnames correspodn to keys
+    var newUsers = {};
+    for (var host in $scope.users) {
+      newUsers[host] = {};
+      for (var key in $scope.users[host]) {
+        var info = $scope.users[host][key];
+        newUsers[host][info._username] = info;
+      }
+    }
+    $scope.users = newUsers;
+    http_post('/api/users', newUsers);
+  };
+
+  $scope.addRepo = function(host) {
+    $scope.repos[host]["new-repo-" + Math.random()] = {
+      force_push_notify: true,
+      jira_enabled: true,
+    };
+  }
+
+  $scope.saveRepos = function() {
+    // remap to make sure edited usersnames correspodn to keys
+    var newRepos = {};
+    for (var host in $scope.repos) {
+      newRepos[host] = {};
+      for (var key in $scope.repos[host]) {
+        var info = $scope.repos[host][key];
+        newRepos[host][info._repo] = info;
+      }
+    }
+    $scope.repos = newRepos;
+    http_post('/api/repos', newRepos);
+  };
+
+  $scope.removeRepo = function(host, repo) {
+    delete $scope.repos[host][repos];
   }
 });
