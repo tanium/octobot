2add2ed3dc6c18b8cc029c9f20f2307911a8809a
Matt Hauck <matt.hauck@tanium.com>
2017-07-27 10:09:05 -0700

    Set umask in docker-entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,9 @@
 set -e
 
 if [[ "$1" = "octobot" ]] || [[ "$1" = "octobot-passwd" ]]; then
+  umask 0077
   chown -R octobot /data
+  exec gosu octobot sh -c 'rm /data/foo || echo "no foo"; touch /data/foo; ls -l /data/foo'
   exec gosu octobot $@
 fi
 
