fc20c89ecfaaac535aa2b33b3e50ac23d0ea547c
Matt Hauck <matt.hauck@tanium.com>
2017-10-13 23:32:00 -0700

    Add version.rs to compare versions semantically
    Feels a bit like re-inventing the wheel, but there doesn't appear
    to be a crate out there that does we want. Sadly `semver` does
    not accept a fourth element in the version tuple.

---
+++ src/version.rs
@@ -0,0 +1,172 @@
+use std::cmp::Ordering;
+use std::fmt;
+
+#[derive(Clone, Eq, Debug)]
+pub struct Version {
+    parts: Vec<u32>,
+}
+
+impl Version {
+    pub fn parse(version_str: &str) -> Option<Version> {
+        let parts = version_str.split('.').map(|p| p.parse::<u32>()).collect::<Vec<_>>();
+        if parts.iter().any(|ref p| p.is_err()) {
+            return None;
+        }
+        let mut parts: Vec<u32> = parts.into_iter().map(|p| p.unwrap()).collect();
+        while parts.len() < 4 {
+            parts.push(0)
+        }
+
+        Some(Version {
+            parts: parts
+        })
+    }
+
+    pub fn to_string(&self) -> String {
+        self.parts.iter().map(|p| p.to_string()).collect::<Vec<_>>().join(".")
+    }
+
+    pub fn major(&self) -> u32 {
+        assert!(self.parts.len() >= 4);
+        self.parts[0]
+    }
+
+    pub fn minor(&self) -> u32 {
+        assert!(self.parts.len() >= 4);
+        self.parts[1]
+    }
+}
+
+impl fmt::Display for Version {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.to_string())
+    }
+}
+
+impl PartialEq for Version {
+    fn eq(&self, other: &Version) -> bool {
+        self.parts == other.parts
+    }
+}
+
+impl PartialOrd for Version {
+    fn partial_cmp(&self, other: &Version) -> Option<Ordering> {
+        // Note: versions should almost always have exactly 4 parts.
+        // The constructor enforces at least 4 parts.
+        //
+        for i in 0..self.parts.len() {
+            if i >= other.parts.len() {
+                return None;
+            }
+            if self.parts[i] < other.parts[i] {
+                return Some(Ordering::Less)
+            } else if self.parts[i] > other.parts[i] {
+                return Some(Ordering::Greater)
+            }
+        }
+
+        if self.parts.len() == other.parts.len() {
+            return Some(Ordering::Equal)
+        }
+
+        return None
+    }
+}
+
+impl Ord for Version {
+    fn cmp(&self, other: &Version) -> Ordering {
+        match self.partial_cmp(other) {
+            Some(o) => o,
+            None => {
+                if self.parts.len() > other.parts.len() {
+                    Ordering::Greater
+                } else {
+                    Ordering::Less
+                }
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_version_parse() {
+        assert_eq!("1.2.3.5", Version::parse("1.2.3.5").unwrap().to_string());
+        assert_eq!("1.2.3.0", Version::parse("1.2.3").unwrap().to_string());
+        assert_eq!("1.2.0.0", Version::parse("1.2").unwrap().to_string());
+    }
+
+    #[test]
+    fn test_version_equal() {
+        assert!(Version::parse("1.0").unwrap() == Version::parse("1.0.0").unwrap());
+        assert!(!(Version::parse("1.0").unwrap() == Version::parse("1.1").unwrap()));
+    }
+
+    #[test]
+    fn test_version_not_equal() {
+        assert!(Version::parse("1.0").unwrap() != Version::parse("2.0.0").unwrap());
+        assert!(!(Version::parse("1.0").unwrap() != Version::parse("1.0.0").unwrap()));
+    }
+
+    #[test]
+    fn test_version_less() {
+        // lesser first digit
+        assert!(Version::parse("1.0.0.0").unwrap() < Version::parse("2.0.0.0").unwrap());
+        // lesser second digit
+        assert!(Version::parse("1.0.0.0").unwrap() < Version::parse("1.1.0.0").unwrap());
+        // lesser third digit
+        assert!(Version::parse("1.0.0.0").unwrap() < Version::parse("1.0.1.0").unwrap());
+        // lesser third digit
+        assert!(Version::parse("1.0.0.0").unwrap() < Version::parse("1.0.0.1").unwrap());
+        // negative test
+        assert!(!(Version::parse("2.0.0.0").unwrap() < Version::parse("1.0.0.1").unwrap()));
+    }
+
+    #[test]
+    fn test_version_less_or_equal() {
+        assert!(Version::parse("1.0.0.0").unwrap() <= Version::parse("1.0.0.1").unwrap());
+        assert!(Version::parse("1.0.0.0").unwrap() <= Version::parse("1.0.0.0").unwrap());
+        assert!(!(Version::parse("2.0.0.1").unwrap() <= Version::parse("1.0.0.0").unwrap()));
+    }
+
+    #[test]
+    fn test_version_greater() {
+        assert!(Version::parse("4.8").unwrap() > Version::parse("4.1.2").unwrap());
+        assert!(!(Version::parse("4.8").unwrap() > Version::parse("4.9").unwrap()));
+    }
+
+    #[test]
+    fn test_version_greater_or_equal() {
+        assert!(Version::parse("4.8.1").unwrap() >= Version::parse("4.8").unwrap());
+        assert!(Version::parse("4.8").unwrap() >= Version::parse("4.8").unwrap());
+        assert!(!(Version::parse("4.8").unwrap() >= Version::parse("4.9").unwrap()));
+    }
+
+    #[test]
+    fn test_version_sort() {
+        let mut versions = vec![
+            Version::parse("1.2.0.0").unwrap(),
+            Version::parse("1.2.3.4").unwrap(),
+            Version::parse("1.2.3.0").unwrap(),
+            Version::parse("1.0.0.0").unwrap(),
+        ];
+        versions.sort();
+
+        assert_eq!(vec![
+            Version::parse("1.0.0.0").unwrap(),
+            Version::parse("1.2.0.0").unwrap(),
+            Version::parse("1.2.3.0").unwrap(),
+            Version::parse("1.2.3.4").unwrap(),
+        ], versions);
+    }
+
+    #[test]
+    fn test_version_max() {
+        let versions = vec![Version::parse("1.0.0.0").unwrap(), Version::parse("2.0.0.0").unwrap()];
+        assert_eq!(&Version::parse("2.0.0.0").unwrap(), versions.iter().max().unwrap());
+        assert_eq!(&Version::parse("1.0.0.0").unwrap(), versions.iter().min().unwrap());
+    }
+}
