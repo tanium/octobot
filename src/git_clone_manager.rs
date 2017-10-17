e656f68c7bae293505fc21cb897949ae9d6a8f89
Matt Hauck <matthauck@gmail.com>
2017-10-16 22:11:01 -0700

    Prune old tags on fetch to avoid stale tags and bad versions (#118)

--- src/git_clone_manager.rs
+++ src/git_clone_manager.rs
@@ -42,7 +42,8 @@ impl GitCloneManager {
                            clone_dir);
 
         if clone_dir.join(".git").exists() {
-            try!(git.run(&["fetch", "--prune"]));
+            // prune local tags deleted from remotes: important to avoid stale/bad version tags
+            try!(git.run(&["fetch", "--prune", "+refs/tags/*:refs/tags/*"]));
         } else {
             if let Err(e) = fs::create_dir_all(&clone_dir) {
                 return Err(format!("Error creating clone directory '{:?}': {}", clone_dir, e));
