389c16e21f9906670c9348279c308575511b0dd3
Matt Hauck <matt.hauck@tanium.com>
2017-10-23 17:48:24 -0700

    Support HTTPS
    This avoids needing nginx/apache as a reverse-proxy in front of
    octobot just to get HTTPS working.

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -4,11 +4,22 @@ mod github_verify;
 mod html_handler;
 mod http;
 mod octobot_service;
+mod redirect_service;
 pub mod login;
 mod sessions;
 
+use std::fs;
+use std::io;
+use std::io::Seek;
+use std::net::SocketAddr;
+use std::sync::Arc;
+use std::thread;
 
 use hyper::server::Http;
+use rustls;
+use rustls::internal::pemfile;
+use tokio_proto;
+use tokio_rustls;
 
 use self::octobot_service::OctobotService;
 use self::sessions::Sessions;
@@ -17,9 +28,8 @@ use github;
 use github::api::GithubSession;
 use jira;
 use jira::api::JiraSession;
+use server::redirect_service::RedirectService;
 use server::github_handler::GithubHandlerState;
-use std::sync::Arc;
-
 
 pub fn start(config: Config) -> Result<(), String> {
     let config = Arc::new(config);
@@ -39,26 +49,91 @@ pub fn start(config: Config) -> Result<(), String> {
         jira = None;
     }
 
-    let addr = match config.main.listen_addr {
+    let http_addr: SocketAddr = match config.main.listen_addr {
         Some(ref addr_and_port) => addr_and_port.parse().unwrap(),
         None => "0.0.0.0:3000".parse().unwrap(),
     };
 
+    let https_addr: SocketAddr = match config.main.listen_addr_ssl {
+        Some(ref addr_and_port) => addr_and_port.parse().unwrap(),
+        None => "0.0.0.0:3001".parse().unwrap(),
+    };
+
+    let tls;
+    if let Some(ref cert_file) = config.main.ssl_cert_file {
+        if let Some(ref key_file) = config.main.ssl_key_file {
+            let key = load_private_key(key_file);
+            let certs = load_certs(cert_file);
+
+            let mut the_cfg = rustls::ServerConfig::new();
+            the_cfg.set_single_cert(certs, key);
+
+            tls = Some(Arc::new(the_cfg));
+        } else {
+            warn!("Warning: No SSL configured");
+            tls = None;
+        }
+    } else {
+        warn!("Warning: No SSL configured");
+        tls = None;
+    }
+
     let ui_sessions = Arc::new(Sessions::new());
     let github_handler_state = Arc::new(GithubHandlerState::new(config.clone(), github.clone(), jira.clone()));
 
-    let server = Http::new()
-        .bind(&addr, move || {
-            Ok(OctobotService::new(
-                config.clone(),
-                ui_sessions.clone(),
-                github_handler_state.clone(),
-            ))
-        })
-        .unwrap();
+    match tls {
+        Some(tls) => {
+            let https = thread::spawn(move || {
+                let server = tokio_proto::TcpServer::new(tokio_rustls::proto::Server::new(Http::new(), tls), https_addr.clone());
+                info!("Listening (HTTPS) on {}", https_addr);
+                server.serve( move || {
+                    Ok(OctobotService::new(config.clone(), ui_sessions.clone(), github_handler_state.clone()))
+                });
+            });
+            let http = thread::spawn(move || {
+                let server = Http::new().bind(&http_addr, move || {
+                    Ok(RedirectService::new(https_addr.port()))
+                }).unwrap();
 
-    info!("Listening on port {}", addr);
-    server.run().unwrap();
+                info!("Listening (HTTP Redirect) on {}", http_addr);
+                server.run().unwrap();
+            });
+            https.join().unwrap();
+            http.join().unwrap();
+        },
+        None => {
+            let server = Http::new().bind(&http_addr, move || {
+                Ok(OctobotService::new(config.clone(), ui_sessions.clone(), github_handler_state.clone()))
+            }).unwrap();
+
+            info!("Listening (HTTP) on {}", http_addr);
+            server.run().unwrap();
+         }
+    };
 
     Ok(())
+}
+
+fn load_certs(filename: &str) -> Vec<rustls::Certificate> {
+    let certfile = fs::File::open(filename).expect("cannot open certificate file");
+    let mut reader = io::BufReader::new(certfile);
+    pemfile::certs(&mut reader).unwrap()
+}
+
+fn load_private_key(filename: &str) -> rustls::PrivateKey {
+    let keyfile = fs::File::open(filename).expect("cannot open private key file");
+    let mut reader = io::BufReader::new(keyfile);
+
+    let keys = pemfile::rsa_private_keys(&mut reader).unwrap();
+    if keys.len() == 1 {
+        return keys[0].clone()
+    }
+
+    reader.seek(io::SeekFrom::Start(0)).unwrap();
+    let keys = pemfile::pkcs8_private_keys(&mut reader).unwrap();
+    if keys.len() == 1 {
+        return keys[0].clone()
+    }
+
+    panic!("Unable to find private key in file {}", filename);
 }
---
+++ src/server/redirect_service.rs
@@ -0,0 +1,52 @@
+use futures::future::{self, FutureResult};
+use hyper;
+use hyper::{StatusCode, Uri};
+use hyper::header::{Host, Location};
+use hyper::server::{Request, Response, Service};
+
+pub struct RedirectService {
+    https_port: u16,
+}
+
+impl RedirectService {
+    pub fn new(https_port: u16) -> RedirectService {
+        RedirectService { https_port: https_port }
+    }
+
+    fn rewrite_uri(&self, uri: Uri, host_header: Option<&Host>) -> String {
+        let mut new_url = String::from("https://");
+        if let Some(host) = uri.host() {
+            new_url += host;
+            self.maybe_add_port(&mut new_url, uri.port())
+        } else if let Some(host) = host_header {
+            new_url += host.hostname();
+            self.maybe_add_port(&mut new_url, host.port());
+        }
+        new_url += uri.path();
+        if let Some(q) = uri.query() {
+            new_url += &format!("?{}", q);
+        }
+
+        new_url
+    }
+
+    fn maybe_add_port(&self, new_url: &mut String, req_port: Option<u16>) {
+        // if port was specified, then not using docker or otherwise to remap ports --> substitute explicit port
+        if req_port.is_some() {
+            new_url.push_str(&format!(":{}", self.https_port));
+        }
+    }
+}
+
+impl Service for RedirectService {
+    type Request = Request;
+    type Response = Response;
+    type Error = hyper::Error;
+    type Future = FutureResult<Response, hyper::Error>;
+
+    fn call(&self, req: Request) -> Self::Future {
+        let new_uri = self.rewrite_uri(req.uri().clone(), req.headers().get::<Host>());
+        debug!("Redirecting request to {}", new_uri);
+        future::ok(Response::new().with_status(StatusCode::Found).with_header(Location::new(new_uri)))
+    }
+}
