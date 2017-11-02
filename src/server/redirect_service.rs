b65e21889ad6e06beb6bedfc0661fd4fc475da78
Matt Hauck <matt.hauck@tanium.com>
2017-11-01 18:24:48 -0700

    test it!

--- src/server/redirect_service.rs
+++ src/server/redirect_service.rs
@@ -47,6 +47,68 @@ impl Service for RedirectService {
     fn call(&self, req: Request) -> Self::Future {
         let new_uri = self.rewrite_uri(req.uri().clone(), req.headers().get::<Host>());
         debug!("Redirecting request to {}", new_uri);
-        future::ok(Response::new().with_status(StatusCode::MovedPermanently).with_header(Location::new(new_uri)))
+        future::ok(Response::new().with_status(StatusCode::MovedPermanently).with_header(
+            Location::new(new_uri),
+        ))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::str::FromStr;
+
+    use hyper::header::Headers;
+
+    #[test]
+    fn test_rewrite_uri_uri_host_primary() {
+        let service = RedirectService::new(99);
+        let uri = Uri::from_str("http://host.foo.com/path/to/thing?param=value&param2=value2").unwrap();
+        let mut headers = Headers::new();
+        headers.set(Host::new("other.com", None));
+
+        assert_eq!(
+            "https://host.foo.com/path/to/thing?param=value&param2=value2",
+            service.rewrite_uri(uri, headers.get::<Host>())
+        );
+    }
+
+    #[test]
+    fn test_rewrite_uri_header_host_secondary() {
+        let service = RedirectService::new(99);
+        let uri = Uri::from_str("/path/to/thing?param=value&param2=value2").unwrap();
+        let mut headers = Headers::new();
+        headers.set(Host::new("other.com", None));
+
+        assert_eq!(
+            "https://other.com/path/to/thing?param=value&param2=value2",
+            service.rewrite_uri(uri, headers.get::<Host>())
+        );
+    }
+
+    #[test]
+    fn test_rewrite_uri_includes_port_if_uri_has_port() {
+        let service = RedirectService::new(99);
+        let uri = Uri::from_str("http://host.foo.com:20/path/to/thing?param=value&param2=value2").unwrap();
+        let mut headers = Headers::new();
+        headers.set(Host::new("other.com", None));
+
+        assert_eq!(
+            "https://host.foo.com:99/path/to/thing?param=value&param2=value2",
+            service.rewrite_uri(uri, headers.get::<Host>())
+        );
+    }
+
+    #[test]
+    fn test_rewrite_uri_includes_port_if_header_has_port() {
+        let service = RedirectService::new(99);
+        let uri = Uri::from_str("/path/to/thing?param=value&param2=value2").unwrap();
+        let mut headers = Headers::new();
+        headers.set(Host::new("other.com", Some(20)));
+
+        assert_eq!(
+            "https://other.com:99/path/to/thing?param=value&param2=value2",
+            service.rewrite_uri(uri, headers.get::<Host>())
+        );
     }
 }
