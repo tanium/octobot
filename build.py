78d20903630f3ec06919dbb2d1ca43bbd971f306
Matt Hauck <matt.hauck@tanium.com>
2017-11-03 12:25:43 -0700

    and not sure underscores are necessary

--- build.py
+++ build.py
@@ -34,16 +34,16 @@ if os.path.exists(docker_tmp):
     shutil.rmtree(docker_tmp)
 os.makedirs(docker_out)
 
-with task("Dockerfile.build") as _:
+with task("Dockerfile.build"):
     run("docker build . -f Dockerfile.build -t octobot:build")
-with task("run-tests") as _:
+with task("run_tests"):
     run("docker run -t --privileged --rm octobot:build")
-with task("extract-files") as _:
+with task("extract_files"):
     run("docker rm -f extract", ignore_fail=True, quiet=True)
     run("docker create --name extract octobot:build")
     run("docker cp extract:/usr/src/app/target/release/octobot {}".format(docker_out))
     run("docker cp extract:/usr/src/app/target/release/octobot-passwd {}".format(docker_out))
     run("docker cp extract:/usr/src/app/target/release/octobot-ask-pass {}".format(docker_out))
     run("docker rm -f extract")
-with task("Dockerfile") as _:
+with task("Dockerfile"):
     run("docker build . -t octobot:latest")
