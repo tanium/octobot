e0290fe6f25fdaac1d5e7ba73d838733ecd083c0
Matt Hauck <matthauck@gmail.com>
2017-11-03 16:38:57 -0700

    Clean up travis build output (#147)
    - Add travis folds to make build output cleaner
    - Get some color in test output

--- build.py
+++ build.py
@@ -5,6 +5,20 @@ import re
 import shutil
 import subprocess
 
+is_travis = os.environ.get('TRAVIS') is not None
+class task:
+    def __init__(self, title):
+        self.title = title
+    def __enter__(self):
+        if is_travis:
+            print "travis_fold:start:{}".format(self.title)
+        print ">> Starting {}".format(self.title)
+        return self
+    def __exit__(self, type, value, traceback):
+        print ">> Ending {}".format(self.title)
+        if is_travis:
+            print "travis_fold:end:{}".format(self.title)
+
 def run(cmd, ignore_fail=False, quiet=False):
     print ">> {}".format(cmd)
     stdout = subprocess.PIPE if quiet else None
@@ -20,12 +34,16 @@ if os.path.exists(docker_tmp):
     shutil.rmtree(docker_tmp)
 os.makedirs(docker_out)
 
-run("docker build . -f Dockerfile.build -t octobot:build")
-run("docker run --privileged --rm octobot:build")
-run("docker rm -f extract", ignore_fail=True, quiet=True)
-run("docker create --name extract octobot:build")
-run("docker cp extract:/usr/src/app/target/release/octobot {}".format(docker_out))
-run("docker cp extract:/usr/src/app/target/release/octobot-passwd {}".format(docker_out))
-run("docker cp extract:/usr/src/app/target/release/octobot-ask-pass {}".format(docker_out))
-run("docker rm -f extract")
-run("docker build . -t octobot:latest")
+with task("Dockerfile.build"):
+    run("docker build . -f Dockerfile.build -t octobot:build")
+with task("run_tests"):
+    run("docker run -t --privileged --rm octobot:build")
+with task("extract_files"):
+    run("docker rm -f extract", ignore_fail=True, quiet=True)
+    run("docker create --name extract octobot:build")
+    run("docker cp extract:/usr/src/app/target/release/octobot {}".format(docker_out))
+    run("docker cp extract:/usr/src/app/target/release/octobot-passwd {}".format(docker_out))
+    run("docker cp extract:/usr/src/app/target/release/octobot-ask-pass {}".format(docker_out))
+    run("docker rm -f extract")
+with task("Dockerfile"):
+    run("docker build . -t octobot:latest")
