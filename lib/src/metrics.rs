c109f7fc7f4afe43efc84529415de66dae30e491
Trever Shick <trever.shick@tanium.com>
2021-10-22 09:13:25 -0500

    chore(metrics): use process_collector on linux only
    The process collector is only used in linux, but the 'use' statement was
    not conditional so the build would fail on mac.

--- lib/src/metrics.rs
+++ lib/src/metrics.rs
@@ -1,4 +1,6 @@
+#[cfg(target_os = "linux")]
 use prometheus::process_collector::ProcessCollector;
+
 use prometheus::Registry;
 use prometheus::{register_gauge_vec_with_registry, GaugeVec};
 use prometheus::{register_gauge_with_registry, Gauge};
