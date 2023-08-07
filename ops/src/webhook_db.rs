176a1c5bce6378f8a2fe8065a1a25e4f9de99c20
Matt Hauck <matt.hauck@tanium.com>
2023-08-07 15:40:00 -0700

    update

--- ops/src/webhook_db.rs
+++ ops/src/webhook_db.rs
@@ -1,7 +1,7 @@
 use std::sync::Mutex;
 use std::time::SystemTime;
 
-use failure::format_err;
+use anyhow::anyhow;
 use rusqlite::types::ToSql;
 
 use octobot_lib::db::{migrations, Database};
@@ -33,7 +33,7 @@ impl WebhookDatabase {
             .prepare("SELECT guid FROM processed_webhooks ORDER BY timestamp desc LIMIT 1000")?;
         let found = stmt
             .query_map([], |row| row.get(0))
-            .map_err(|e| format_err!("Error fetching webhooks: {}", e))?;
+            .map_err(|e| anyhow!("Error fetching webhooks: {}", e))?;
 
         let mut recent_events = vec![];
         for event in found {
@@ -70,7 +70,7 @@ impl WebhookDatabase {
             "INSERT INTO processed_webhooks (guid, timestamp) VALUES (?1, ?2)",
             [&guid, &timestamp as &dyn ToSql],
         )
-        .map_err(|e| format_err!("Error inserting webhook {}: {}", guid, e))?;
+        .map_err(|e| anyhow!("Error inserting webhook {}: {}", guid, e))?;
 
         Ok(())
     }
@@ -94,7 +94,7 @@ impl WebhookDatabase {
         let conn = data.db.connect()?;
         let mut stmt = conn.prepare("SELECT 1 FROM processed_webhooks where guid = ?1")?;
 
-        stmt.exists([&guid]).map_err(|e| format_err!("{}", e))
+        stmt.exists([&guid]).map_err(|e| anyhow!("{}", e))
     }
 
     pub fn clean(&self, expiration: SystemTime) -> Result<()> {
@@ -109,7 +109,7 @@ impl WebhookDatabase {
             "DELETE FROM processed_webhooks where timestamp < ?1",
             [&deadline as &dyn ToSql],
         )
-        .map_err(|e| format_err!("Error cleaning webhook db: {}", e))?;
+        .map_err(|e| anyhow!("Error cleaning webhook db: {}", e))?;
 
         Ok(())
     }
@@ -120,10 +120,10 @@ mod tests {
     use std::{path::PathBuf, time::Duration};
 
     use super::*;
-    use tempdir::TempDir;
+    use tempfile::{tempdir, TempDir};
 
     fn new_test() -> (WebhookDatabase, PathBuf, TempDir) {
-        let temp_dir = TempDir::new("webhook_db.rs").unwrap();
+        let temp_dir = tempdir().unwrap();
         let db_file = temp_dir.path().join("webhook.sqlite3");
         let db = WebhookDatabase::new(&db_file.to_string_lossy()).expect("create temp database");
 
